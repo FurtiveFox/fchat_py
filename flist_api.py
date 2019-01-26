ee093ceb186f5bf38563954a5ddafdfc157c9c6a
FurtiveFox <furtivefox@gmail.com>
2019-01-26 10:46:50 -0600

    started web api framework

---
+++ flist_api.py
@@ -0,0 +1,34 @@
+import requests
+import json
+
+from constants import AppInfo as A
+from constants import FlistEndpoints as E
+
+
+
+
+class FlistAPI:
+
+    def __init__(self, account):
+
+        self.account_name = account.account_name
+        self.password = account.password
+        self.s = requests.Session()
+        self.s.headers.update({'User-Agent': A.USERAGENT})
+
+    def init_auth(self):
+        """Authenticates with f-list web api to get full acount info"""
+        payload = {'account': self.account_name, 'password': self.userpass, 'new_character_list': "true"}
+        try:
+            response = self.s.post(E.domain + E.getApiTicket, params=payload)
+            response_dict = json.loads(response.text)
+            self.acct_dict = response_dict
+        except requests.exceptions.RequestException as e:
+            print(e)
+
+    def get_ticket(self):
+        """Authenticates with f-list web api and requests minimal info"""
+        payload = {'account': self.account_name, 'password': self.userpass, 'no_characters': 'true', 'no_friends': 'true', 'no_bookmarks': 'true'}
+        response = self.s.post(E.domain + E.getApiTicket, params=payload)
+        response_dict = json.loads(response.text)
+        self.ticket = response_dict['ticket']
