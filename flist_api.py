d3a09ecf586e515b52cf561efa53f075133b2d93
FurtiveFox <furtivefox@gmail.com>
2019-02-03 23:13:11 -0600

    working api routines

--- flist_api.py
+++ flist_api.py
@@ -1,5 +1,8 @@
 import requests
 import json
+import urllib
+import urllib.request
+import urllib.parse
 
 from constants import AppInfo as A
 from constants import FlistEndpoints as E
@@ -21,6 +24,7 @@ class FlistAPI:
         payload = {'account': self.account_name, 'password': self.password, 'new_character_list': "true"}
         try:
             response = self.s.post(E.DOMAIN + E.GETAPITICKET, params=payload)
+            print(response.url)
             response_dict = json.loads(response.text)
             self.acct_dict = response_dict
         except requests.exceptions.RequestException as e:
@@ -30,5 +34,28 @@ class FlistAPI:
         """Authenticates with f-list web api and requests minimal info"""
         payload = {'account': self.account_name, 'password': self.password, 'no_characters': 'true', 'no_friends': 'true', 'no_bookmarks': 'true'}
         response = self.s.post(E.DOMAIN + E.GETAPITICKET, params=payload)
+        print(response.url)
         response_dict = json.loads(response.text)
         self.ticket = response_dict['ticket']
+
+    def get_mapping_data(self):
+
+        payload = {}
+        response = self.s.post(E.DOMAIN + E.MAPPINGLIST, params=payload)
+        response_dict = json.loads(response.text)
+        return response_dict
+
+    def get_character_data(self, character):
+        payload = {'account': self.account_name, 'ticket': self.ticket, 'name': character}
+        response = requests.post(E.DOMAIN + E.CHARACTERDATA, data=payload)
+        print(response.url)
+        response_dict = json.loads(response.text)
+        return response_dict
+
+    def get_character_friends(self, character):
+        payload = {'account': self.account_name, 'ticket': self.ticket, 'name': character}
+        body = {'name': character}
+        response = requests.post(E.DOMAIN + E.CHARACTERFRIENDS, data=payload)
+        print(response.url)
+        response_dict = json.loads(response.text)
+        return response_dict
